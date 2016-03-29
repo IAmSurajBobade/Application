479aae221d8401e6e34be1382e23ae9a0a215843
regotiss <sujata.regoti@gmail.com>
2016-03-29 15:12:17 +0530

    RemoteContacts added

---
+++ app/src/main/java/com/application/sujata/social_me/beans/RemoteContacts.java
@@ -0,0 +1,27 @@
+package com.application.sujata.social_me.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sujata on 29/3/16.
+ */
+public class RemoteContacts {
+    private static List<MemberInfo> contacts;
+
+    static {
+        contacts = new ArrayList<>();
+    }
+    public static void setContacts(List<MemberInfo> contcts){
+        contacts = contcts;
+    }
+    public static List<MemberInfo> getContacts(){
+        return contacts;
+    }
+    public static void addToContacts(MemberInfo post){
+        contacts.add(post);
+    }
+    public static void clearContacts(){
+        contacts.clear();
+    }
+}
